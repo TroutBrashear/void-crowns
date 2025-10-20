985ea6623fc55cb71ce88cffc7d2f8ba3bf1da90
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2025-10-20 23:58:44 +0200

    feat(engine): added BFS pathfinding algorithm

---
+++ src/engine/pathfinding.ts
@@ -0,0 +1,42 @@
+import type { System } from '../types/gameState';
+
+export function findPath(startingSystemId: number, endingSystemId: number, systems: EntitiesState<System>): number[] {
+	let finalPath: number[] = [];
+
+	//set up for BFS
+	const queue: number[] = [];
+	const cameFrom: { [key: number]: number | null } = {};
+	queue.push(startingSystemId);
+	cameFrom[startingSystemId] = null;
+
+	while(queue.length > 0){
+
+		const currentSystemId = queue.shift()!; 
+		const currentSystem = systems.entities[currentSystemId];
+		if(currentSystemId === endingSystemId){
+			break;
+		}
+		
+		for(const adjacentId of currentSystem.adjacentSystemIds){
+			if(!(adjacentId in cameFrom)){
+				queue.push(adjacentId);
+				cameFrom[adjacentId] = currentSystemId;
+			}
+		}
+	}
+
+	if (!(endId in cameFrom)) {
+    	return []; // No path exists
+  	}
+
+  	let currentSystemId = endingSystemId;
+
+  	while(currentSystemId !== null){
+  		finalPath.push(currentSystemId);
+  		currentSystemId = cameFrom[currentSystemId];
+  	}
+
+  	finalPath.reverse();
+
+	return finalPath.slice(1);
+}
