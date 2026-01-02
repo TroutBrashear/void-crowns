bb23480f8dcadfecb9f0918a65b900de1e283e14
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2026-01-02 12:44:52 -0500

    refactor(engine): refactor of resolveBattle and processCombat to only consider two orgs at once - this eliminates the logical error that destroyed all non-winner fleets after battles. Closes #1

--- src/engine/combat.ts
+++ src/engine/combat.ts
@@ -7,42 +7,45 @@ function getFleetsInSystem (currentState: GameState, systemId: number): Fleet[] 
 }
 
 
-function resolveBattle(currentState: GameState, fleetsInSystem: Fleet[]): EngineResult {
-	let fleetScore = new Map<number, number>();
+function resolveBattle(currentState: GameState, fleetsInSystemFactionA: Fleet[], fleetsInSystemFactionB: Fleet[]): EngineResult {
+	let fleetScoreA = 0;
+	let fleetScoreB = 0;
 
-
-	for(const fleet of fleetsInSystem){
+	for(const fleet of fleetsInSystemFactionA){
 		if (!fleet) {
 			continue;
 		}
-
-		const currentScore = fleetScore.get(fleet.ownerNationId) || 0;
-
-		let updateScore = currentScore + Math.floor(Math.random() * 20);
-
-		fleetScore.set(fleet.ownerNationId, updateScore);
+		fleetScoreA += Math.floor(Math.random() * 20);
 	}
+	for(const fleet of fleetsInSystemFactionB){
+		if (!fleet) {
+			continue;
+		}
+		fleetScoreB += Math.floor(Math.random() * 20);
+	}
+	
+	const fleetEntities = { ...currentState.fleets.entities };
+	const fleetIds = [...currentState.fleets.ids];
+	 const participatingFactions = [fleetsInSystemFactionA[0].ownerNationId, fleetsInSystemFactionB[0].ownerNationId];
 
 	let winnerId = -1;
 	let winnerScore = 0;
 	//determine winner
-	for(const [factionId, score] of fleetScore) {
-		if(score > winnerScore) {
-			winnerId = factionId;
-			winnerScore = score; 
-		}
-	}
-	
-	const remainingFleetIds = fleetsInSystem.filter(fleet => fleet.ownerNationId === winnerId).map(fleet => fleet.id);
-
-	const fleetEntities = { ...currentState.fleets.entities };
-	const fleetIds = [...currentState.fleets.ids];
-	for(const fleet of fleetsInSystem){
-		if(!remainingFleetIds.includes(fleet.id)){
+	if(fleetScoreA > fleetScoreB){
+		winnerId = fleetsInSystemFactionA[0].ownerNationId;
+		for(const fleet of fleetsInSystemFactionB){	
 			delete fleetEntities[fleet.id];
 			fleetIds.splice(fleetIds.indexOf(fleet.id), 1);
 		}
 	}
+	else{
+		winnerId = fleetsInSystemFactionB[0].ownerNationId;
+		for(const fleet of fleetsInSystemFactionA){	
+			delete fleetEntities[fleet.id];
+			fleetIds.splice(fleetIds.indexOf(fleet.id), 1);
+		}
+	}
+	
 
 	//newState to be returned
 	const newState = {
@@ -54,14 +57,13 @@ function resolveBattle(currentState: GameState, fleetsInSystem: Fleet[]): Engine
 	};
 	
 	//collect info to build result event
-    const participatingFactions = Array.from(fleetScore.keys());
     const winnerOrg = currentState.orgs.entities[winnerId];
 	const events: GameEvent[] = [];
 
 	const battleResultEvent: GameEvent = {
     	type: 'battle_result',
     	message: `Battle concluded! The ${winnerOrg.flavor.name} forces are victorious.`,
-    	locationId: fleetsInSystem[0]?.locationSystemId, 
+    	locationId: fleetsInSystemFactionA[0]?.locationSystemId, 
     	involvedOrgIds: participatingFactions,
     	isPlayerVisible: participatingFactions.includes(1),
  	};
@@ -82,7 +84,9 @@ export function processCombat(currentState: GameState): EngineResult {
 	for(const systemId of currentState.systems.ids) {
 		const fleetsInSystem = getFleetsInSystem(currentState, systemId);
 
-		if(fleetsInSystem.length < 1) {
+
+		//no chance for combat if there aren't multiple fleets
+		if(fleetsInSystem.length < 2) {
 			continue;
 		}
 
@@ -102,7 +106,14 @@ export function processCombat(currentState: GameState): EngineResult {
 					const relationship = getRelationship(nextState, firstOrgId, secondOrgId);
 
 					if(relationship.status === 'war'){
-						isBattle = true;
+						//set up battle and call resolveBattle
+						const firstOrgFleets = fleetsInSystem.filter(fleet => fleet.ownerNationId === firstOrgId);
+						const secondOrgFleets = fleetsInSystem.filter(fleet => fleet.ownerNationId === secondOrgId);
+						
+						const battleResult = resolveBattle(nextState, firstOrgFleets, secondOrgFleets);
+						nextState = battleResult.newState;						
+						allCombatEvents.push(...battleResult.events);
+						isBattle=true;
 						break;
 					}
 				}
@@ -110,12 +121,6 @@ export function processCombat(currentState: GameState): EngineResult {
 				if (isBattle) {
 					break;
 				}
-			}
-			//resolve the battle in this system
-			if(isBattle){
-				const battleResult = resolveBattle(nextState, fleetsInSystem);
-				nextState = battleResult.newState;
-				allCombatEvents.push(...battleResult.events);
 			}
 		}
 	}
