3a09665d4ac5571bcc91924792806c666e54171e
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2026-01-25 16:44:21 -0500

    fix(ai): corrected typo, array.find in evaluateAiRelations function

--- src/engine/ai/diplomacy.ts
+++ src/engine/ai/diplomacy.ts
@@ -23,21 +23,21 @@ export function evaluateDiploRequest(currentState: GameState, orgId: number, req
     }
 }
 
-export function evaluateAiRelations(currentState: GameState, currentOrgId: orgId, targetOrgId: targetOrgId): DiploType | null {
+export function evaluateAiRelations(currentState: GameState, currentOrgId: number, targetOrgId: number): DiploType | null {
     const currentOrg = currentState.orgs.entities[currentOrgId];
 
-    const currentRelations = currentOrg.diplomacy.relations[targetOrgId]
+    const currentRelations = currentOrg.diplomacy.relations.find(rel => rel.targetOrgId === targetOrgId);
 
     //does the org want peace?
     if(currentRelations.status == 'war'){
-        if(currentState.intelligence.trueStatus[orgId].militaryStrength < 0.75 * currentState.intelligence.trueStatus[targetOrgId].militaryStrength){ //todo: this logic will lead to infinity wars right now
+        if(currentState.intelligence.trueStatus[currentOrgId].militaryStrength < 0.75 * currentState.intelligence.trueStatus[targetOrgId].militaryStrength){ //todo: this logic will lead to infinity wars right now
             return 'peace';
         }
     }
 
     //does the org want war?
     if(currentRelations.status == 'peace'){
-        if(currentState.intelligence.trueStatus[orgId].militaryStrength > 1.2 * currentState.intelligence.trueStatus[targetOrgId].militaryStrength){
+        if(currentState.intelligence.trueStatus[currentOrgId].militaryStrength > 1.2 * currentState.intelligence.trueStatus[targetOrgId].militaryStrength){
             return 'war';
         }
     }
