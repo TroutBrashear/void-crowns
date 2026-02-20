4840c071101f3f8856c33598a345bcd7f4b92940
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2026-02-19 23:25:37 -0500

    data: added character events. closes #15

--- src/data/events.ts
+++ src/data/events.ts
@@ -29,5 +29,41 @@ export const CHARACTER_EVENTS: Record<number, EventDefinition> = {
 			addSkill: { navalCombat: 1 } ,
 		} as CharProcess,
 		logText: "attended naval training",
-	}
+	},
+	2: { //add 1 academics skill
+		id: 2,
+		type: 'character',
+
+		effect: {
+			addSkill: { academics: 1 } ,
+		} as CharProcess,
+		logText: "attended university schooling",
+	},
+	3: { //add 1 administration skill
+		id: 3,
+		type: 'character',
+
+		effect: {
+			addSkill: { administration: 1 },
+		} as CharProcess,
+		logText: "went to a managment seminar",
+	},
+	4: {
+		id: 4,
+		type: 'character',
+
+		effect: {
+			addSkill: { exploration: 1 },
+		} as CharProcess,
+		logText: "attended ranger training",
+	},
+	5: {
+		id: 5,
+		type: 'character',
+
+		effect: {
+			reduceSkill: { academics: 1 },
+		} as CharProcess,
+		logText: "failed to keep up with academic journals",
+	},
 }
