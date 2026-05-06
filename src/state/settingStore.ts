787d99ddf0607edb6ff057ee96078989d8e35e59
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2026-05-06 00:04:01 -0400

    fix(state): missing )

--- src/state/settingStore.ts
+++ src/state/settingStore.ts
@@ -18,7 +18,7 @@ export const useSettingStore = create<SettingStoreState>((set, get) => {
                          userPronoun: pronoun,
                     },
                 };
-             };
+             });
         },
     };
 });
