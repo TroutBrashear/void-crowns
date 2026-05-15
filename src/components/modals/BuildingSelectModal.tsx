b9dc9995d06466aba2d43e5730024e75b5d95981
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2026-05-14 21:12:05 -0400

    feat(ui): added assign academyPresident ui to BuildingSelectModal

--- src/components/modals/BuildingSelectModal.tsx
+++ src/components/modals/BuildingSelectModal.tsx
@@ -16,13 +16,23 @@ function BuildingSelectModal() {
     ? getBuildingById(selection.id)
     : null;
 
+
     if(!buildingToShow){
         return null;
     }
 
+    const assignedCharacter = buildingToShow.assignedCharacter ? getCharacterById(buildingToShow.assignedCharacter) : null;
+
+
     return (
         <div className={styles.modal}>
             <h2>Building: {buildingToShow.type}</h2>
+            {assignedCharacter && buildingToShow.type === 'navalAcademy' &&
+                <div>
+                    <p>Academy President: { assignedCharacter.name} </p>
+                    {buildingToShow.ownerNationId === 1 && <button onClick={() => openAssignModal("assign_character", {targetId: selection.id, position: "academyPresident"})}>Assign Academy President</button>}
+                </div>
+            }
 
             <button onClick={backModal}>Back</button>
             <button onClick={closeModal}>Close</button>
