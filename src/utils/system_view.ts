741677f8e34f5ba22ef03529acd06679793f4f96
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2026-05-31 23:43:55 -0400

    feat(util): simple util function to arrange systemPlanetoids to resemble tree hierarchy

--- src/utils/system_view.ts
+++ src/utils/system_view.ts
@@ -9,3 +9,28 @@ export function getPlanetoidDepth(planetoids: Record<number, Planetoid>, planeto
 
     return planetaryDepth;
 }
+
+function orderChildren(planetoids: Planetoid[], parent: Planetoid): Planetoid[]{
+    let children: Planetoid[] = [];
+    children.push(parent);
+    for(const planetoid of planetoids){
+        if(planetoid.parentPlanetoidId === parent.id){
+            children = [...children, ...orderChildren(planetoids, planetoid)];
+        }
+    }
+
+    return children;
+}
+
+export function hierarchizeSystem(planetoids: Record<number, Planetoid>, systemId: number): Planetoid[] {
+    let systemPlanetoids = Object.values(planetoids).filter(planetoid => planetoid.locationSystemId === systemId);
+
+    let stars = systemPlanetoids.filter(planetoid => planetoid.parentPlanetoidId === null);
+
+    for(const star of stars){
+         systemPlanetoids.push(...orderChildren(systemPlanetoids, star));
+    }
+
+
+    return systemPlanetoids;
+}
