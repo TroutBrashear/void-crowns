8e75a9cea31fc9857d9493b674ed038a6566ee9e
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2026-06-03 19:15:20 -0400

    refactor(data): modified building definition to use Partial<Resources> for cost

--- src/data/buildings.ts
+++ src/data/buildings.ts
@@ -4,7 +4,7 @@ import type { BuildingClass, Resources, Process } from '../types/gameState';
 
 export interface BuildingDefinition {
   type: BuildingClass;
-  cost: Resources;
+  cost: Partial<Resources>;
   process: Process; // What it does (inputs/outputs)
   tags: string[];
 }
@@ -13,63 +13,63 @@ export const BUILDING_CATALOG: Record<BuildingClass, BuildingDefinition> = {
 	
   mine: {
 	type: 'mine',
-	cost: { credits: 500, rocks: 0, consumerGoods: 0, gas: 0 },
+	cost: { credits: 500 },
 	process: 
 		{ output: { rocks: 100 } },
     tags: [],
   },
   gasWell: {
     type: 'gasWell',
-    cost: { credits: 800, rocks: 0, consumerGoods: 0, gas: 0 },
+    cost: { credits: 800 },
     process:
       { output: { gas: 100 } },
       tags: [],
   },
   powerPlant: {
     type: 'powerPlant',
-    cost: { credits: 300, rocks: 0, consumerGoods: 0, gas: 0 },
+    cost: { credits: 300 },
     process: 
       { output: { credits: 50 } },
     tags: [],
   },
   militaryBase: {
     type: 'militaryBase',
-    cost: { credits: 1000, rocks: 0, consumerGoods: 0, gas: 0 },
+    cost: { credits: 1000},
     process: 
       { },
     tags: [],
   },
   researchLab: {
     type: 'researchLab',
-    cost: { credits: 20000, rocks: 0, consumerGoods: 0, gas: 0 },
+    cost: { credits: 20000 },
     process:
       { },
     tags: [],
   },
   consumerCenter: {
     type: 'consumerCenter',
-    cost: { credits: 1000, rocks: 0, consumerGoods: 0, gas: 0 },
+    cost: { credits: 1000},
     process:
       { input: { consumerGoods: 100 }, output: { credits: 400 } },
     tags: [],
   },
   consumerFactory: {
     type: 'consumerFactory',
-    cost: { credits: 1000, rocks: 0, consumerGoods: 0, gas: 0 },
+    cost: { credits: 1000 },
     process:
       { input: { rocks: 50 }, output: { consumerGoods: 100 } },
     tags: [],
   },
   navalAcademy: {
     type: 'navalAcademy',
-    cost: { credits: 20000, rocks: 0, consumerGoods: 0, gas: 0 },
+    cost: { credits: 20000 },
     process:
       { input: { credits: 100 } },
     tags: ["academy"],
   },
   scienceAcademy: {
     type: 'scienceAcademy',
-    cost: { credits: 20000, rocks: 0, consumerGoods: 0, gas: 0 },
+    cost: { credits: 20000 },
     process:
     { input: { credits: 100 } },
     tags: ["academy"],
