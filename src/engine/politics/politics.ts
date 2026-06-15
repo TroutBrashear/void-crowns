94a802c79fa5e2b4e7d3ceefbef94b3c2a8a2aa6
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2026-06-14 23:52:41 -0400

    feat(engine): basic processPolitics function to be run as part of the tick cycles

---
+++ src/engine/politics/politics.ts
@@ -0,0 +1,39 @@
+import type { GameState, EngineResult, GameEvent } from '../../types/gameState';
+import { averagePopHappiness } from '../population';
+import { determinePotentialIdeology, spawnMovement } from './movements';
+
+export function processPolitics(currentState: GameState ): EngineResult {
+    const planetoids = Object.values(currentState.planetoids.entities);
+    const allPoliticsEvents: GameEvent[] = [];
+
+    let movementTargets: number[] = [];
+
+    for(const planetoid of planetoids){
+        if(planetoid.population){
+            const planetoidPopIds = planetoid.population.popIds;
+
+            const planetoidPops = planetoidPopIds.map(popId => currentState.pops.entities[popId]);
+
+            const planetoidHappiness = averagePopHappiness(planetoidPops);
+
+            if(planetoidHappiness < 25){
+                const movementRoll = Math.random() * 10;
+                if(movementRoll > 8){
+                    movementTargets.push(planetoid.id);
+                }
+            }
+        }
+    }
+
+    let nextState = { ...currentState };
+
+
+     for(const target of movementTargets){
+         const targetIdeology = determinePotentialIdeology(nextState);
+
+         nextState = spawnMovement(nextState, target, targetIdeology);
+    }
+
+
+    return { newState: nextState, events: allPoliticsEvents };
+}
