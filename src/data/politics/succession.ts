e8c8e088c9d70c5b301a7c32bee7645eb16e9607
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2026-07-10 00:02:00 -0400

    feat(data): created catalog entry for hereditary succession

--- src/data/politics/succession.ts
+++ src/data/politics/succession.ts
@@ -6,12 +6,34 @@ export interface SuccessionDefinition {
     successionId: string;
 
     category: string;
-    onComplete?: (currentState: GameState, orgId: number) => number; //actually just returning a character id here
+    onComplete: (currentState: GameState, orgId: number) => number; //actually just returning a character id here
 }
 
 
 
 
 export const SUCCESSION_CATALOG: Record<string, SuccessionDefinition> = {
+    "hereditary": {
+        successionId: "hereditary",
+        category: "autocratic",
+        onComplete: (currentState, orgId) => {
+            let functionOrg = { ...currentState.orgs.entities[orgId]};
 
+            if(functionOrg.characters.leaderId){
+                 const leader = currentState.characters.entities[functionOrg.characters.leaderId];
+
+                 for(const characterId of leader.history.childrenIds){
+                     if(characterId && currentState.characters.entities[characterId].status === 'alive'){
+                        return characterId;
+                     }
+                 }
+            }
+
+            else{
+                return 0;
+            }
+
+               return 0;
+        }
+    }
 }
