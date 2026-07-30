84cefb0b5f126812e36233b13c991f113903b260
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2026-07-29 22:49:19 -0400

    content: added four CHARACTER_TRAITS to traits.ts. Closes #89

--- src/data/traits.ts
+++ src/data/traits.ts
@@ -5,9 +5,40 @@ import type { SkillName } from "../types/charState";
 
 
 export interface TraitDefinition {
+    id: string;
     name: string;
     desc: string;
-    SkillDelta: Record<SkillName, number>;
-    ElectionBonus: number;
+    SkillDelta: Partial<Record<SkillName, number>>;
+    ElectionBonus?: number;
     excludes?: string[]; //incompatible traits
+}
+
+
+export const CHARACTER_TRAITS: Record<string, TraitDefinition> = {
+    "studious": {
+        id: "studious",
+        name: "Studious",
+        desc: "This character is dedicated to their studies.",
+        SkillDelta: { 'academics': 1 },
+        excludes: ["lazy"]
+    },
+    "lazy": {
+        id: "lazy",
+        name: "Lazy",
+        desc: "This character does their best to get out of work.",
+        SkillDelta: { 'academics': -2, 'administration': -1, "exploration": -1 },
+        excludes: ["studious"]
+    },
+    "curious": {
+        id: "curious",
+        name: "Curious",
+        desc: "This character is inquisitive and investigates as much as they can.",
+        SkillDelta: { 'exploration': 2 }
+    },
+    "aggressive": {
+        id: "aggressive",
+        name: "Aggressive",
+        desc: "This character usually tries solving problems with force",
+        SkillDelta: { 'navalCombat': 1 }
+    }
 }
