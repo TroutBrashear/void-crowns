57f891eb70c6977a496c8e7d0777ae778d4225a8
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2026-08-03 21:09:26 -0400

    feat(engine): implemented a planetary_election function to the SUCCESSION_CATALOG Closes #138

--- src/data/politics/succession.ts
+++ src/data/politics/succession.ts
@@ -52,7 +52,7 @@ export const SUCCESSION_CATALOG: Record<string, SuccessionDefinition> = {
             let potentialCandidates = [ ...functionOrg.characters.characterPool ];
 
             if(potentialCandidates.length < 1){
-                return 0;
+                return -1;
             }
 
             let candidateScore: Record<number, Candidacy> = {};
@@ -115,6 +115,7 @@ export const SUCCESSION_CATALOG: Record<string, SuccessionDefinition> = {
                 }
             }
 
+             //determine winner
             let winner = 0;
             let winnerScore = 0;
 
@@ -139,7 +140,7 @@ export const SUCCESSION_CATALOG: Record<string, SuccessionDefinition> = {
             let potentialCandidates = [ ...functionOrg.characters.characterPool ];
 
             if(potentialCandidates.length < 1){
-                return 0;
+                return -1;
             }
 
             let candidateScore: Record<number, Candidacy> = {};
@@ -199,6 +200,7 @@ export const SUCCESSION_CATALOG: Record<string, SuccessionDefinition> = {
                 candidateScore[currentFavorite].score++;
             }
 
+             //determine winner
             let winner = 0;
             let winnerScore = 0;
 
@@ -212,6 +214,115 @@ export const SUCCESSION_CATALOG: Record<string, SuccessionDefinition> = {
             return winner;
 
         }
+    },
+    "planetary_election": { //this is supposed to be modeled on Electoral College style. Each Planetoid gets 1 vote, which is decided by the vote winner on the Planetoid itself.
+        successionId: "planetary_election",
+        category: "democratic",
+         onComplete: (currentState, orgId) => {
+             let functionOrg = { ...currentState.orgs.entities[orgId]};
+             let currentLeader = (functionOrg.characters.leaderId) ? { ...currentState.characters.entities[functionOrg.characters?.leaderId]} : null;
+
+             //get candidates (TODO: political parties should exist...)
+             let potentialCandidates = [ ...functionOrg.characters.characterPool ];
+
+             if(potentialCandidates.length < 1){
+                 return -1;
+             }
+
+             let candidateScore: Record<number, Candidacy> = {};
+
+             if(currentLeader && currentLeader.status === 'alive'){
+                 candidateScore[currentLeader.id] = { score: 2, character: currentLeader };
+             }
+
+             for(let i = 0; i < 3; i++){
+                 let addedCandidate = false;
+
+                 for(let j = 0; j < 3; j++){
+                     const candidateRoll = Math.floor(Math.random() * potentialCandidates.length);
+
+                     if(!Object.keys(candidateScore).includes(String(potentialCandidates[candidateRoll]))){
+                         candidateScore[potentialCandidates[candidateRoll]] = { score: 0, character: { ...currentState.characters.entities[potentialCandidates[candidateRoll]]}};
+                         addedCandidate = true;
+                     }
+
+                     if(addedCandidate){
+                         break;
+                     }
+                 }
+             }
+
+             //score candidates
+             for(const planetoidId of functionOrg.ownedPlanetoids){
+                 const planetoid = currentState.planetoids.entities[planetoidId];
+
+                 let planetoidScore: Record<number, number> = {};
+
+                 for(const key in candidateScore){
+                     planetoidScore[key] = 0;
+                }
+
+                 if(!planetoid.population){
+                     continue;
+                 }
+                 for(const popId of planetoid.population.popIds){
+                     let currentFavorite = 0;
+                     let topScore = 0;
+
+                     const pop = currentState.pops.entities[popId];
+
+                     for(const key in candidateScore){
+                         let voteScore = Math.random() * 4;
+
+                         const candidate = currentState.characters.entities[Number(key)];
+
+                         if(pop.feelings.happiness < 50){ //TODO: example modifier, should depend on candidate traits/ideology
+                             voteScore += 1;
+                         }
+
+                         //TODO: Characters have homeworlds, get a slight boost here from it being the same as the pop's home'
+
+                         voteScore += electabilityScore(candidate);
+
+                         if(voteScore > topScore){
+                             currentFavorite = Number(key);
+                             topScore = voteScore;
+                         }
+
+                     }
+
+                     planetoidScore[currentFavorite]++;
+                 }
+
+
+                 //determine local winner
+                 let winner = 0;
+                 let winnerScore = 0;
+
+                 for(const key in candidateScore){
+                     if(planetoidScore[key] > winnerScore){
+                         winner = Number(key);
+                         winnerScore = planetoidScore[key];
+                     }
+                 }
+
+
+                 candidateScore[winner].score++;;
+             }
+
+             //determine winner
+             let winner = 0;
+             let winnerScore = 0;
+
+             for(const key in candidateScore){
+                 if(candidateScore[key].score > winnerScore){
+                     winner = candidateScore[key].character.id;
+                     winnerScore = candidateScore[key].score;
+                 }
+             }
+
+             return winner;
+         }
     },
     "sortition": {
         successionId: "sortition",
