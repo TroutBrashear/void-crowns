d57feeda1db7629f3faab7f39959ce394519513d
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2026-08-03 22:16:17 -0400

    feat(ui): added display of Planetoid status in PlanetoidSelectModal

--- src/components/modals/PlanetoidSelectModal.tsx
+++ src/components/modals/PlanetoidSelectModal.tsx
@@ -62,6 +62,7 @@ function PlanetoidSelectModal() {
       	  <h3>In the System: {parentSystem.name}</h3>
 
       	  {planetToShow.population &&  <p>Population: {planetToShow.population.total}</p>}
+      	  {planetToShow.government && <p>Status: {planetToShow.government.status}</p>}
       	  <button onClick={() => {changeModal('pops_modal', {type: 'planetoid', id: planetToShow.id}); }}>View Pops</button>
 
 		  {parentSystem.ownerNationId === 1 && (<>
--- src/data/politics/succession.ts
+++ src/data/politics/succession.ts
@@ -307,7 +307,7 @@ export const SUCCESSION_CATALOG: Record<string, SuccessionDefinition> = {
                  }
 
 
-                 candidateScore[winner].score++;;
+                 candidateScore[winner].score++;
              }
 
              //determine winner
