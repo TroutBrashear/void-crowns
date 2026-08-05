ef72520d172d37a21681f4056a794062e0e82642
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2026-08-04 22:04:10 -0400

    feat(data): defined each PoliticalStatus in data/politics/status.ts

--- src/data/politics/status.ts
+++ src/data/politics/status.ts
@@ -1,5 +1,5 @@
 
-
+'Core' | 'Colony' | 'Occupied' | 'Anarchy' | 'Martial';
 
 export interface StatusDefinition {
     id: string;
@@ -7,4 +7,33 @@ export interface StatusDefinition {
     defaultSecurity: string;
 
     votingStrength: number; //between 0 and 1 as a multiplier.
+}
+
+
+export const DEFAULT_STATUS_CATALOG: Record<string, StatusDefinition> = {
+    "Core": {
+        id: "Core",
+        defaultSecurity: "Light",
+        votingStrength: 1
+    },
+    "Colony": {
+        id: "Colony",
+        defaultSecurity: "Light",
+        votingStrength: 0.5
+    },
+    "Occupied": {
+        id: "Occupied",
+        defaultSecurity: "Heavy",
+        votingStrength: 0
+    },
+    "Anarchy": {
+        id: "Anarchy",
+        defaultSecurity: "None",
+        votingStrength: 0
+    },
+    "Martial": {
+        id: "Martial",
+        defaultSecurity: "Heavy",
+        votingStrength: 0.5
+    }
 }
