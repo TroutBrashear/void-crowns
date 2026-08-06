6e53fa0539ad7d2a837035f451b4e0af4458a36e
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2026-08-05 22:06:27 -0400

    feat(ui): added fields for succession type and term length to OrgCreation

--- src/components/OrgCreation.tsx
+++ src/components/OrgCreation.tsx
@@ -3,15 +3,19 @@ import { useUiStore } from '../state/uiStore';
 import { useGameStore } from '../state/gameStore';
 
 import { Button } from './pure/Button';
+import { SUCCESSION_CATALOG } from '../data/politics/succession';
 
 export function OrgCreation() {
 
     const [name, setName] = useState('');
     const [color, setColor] = useState('#ff0000');
     const [speciesName, setSpeciesName] = useState('');
+    const [selectedSuccession, setSelectedSuccession] = useState<string | null>(null);
+    const [termLength, setTermLength] = useState('');
 
     const setAppState = useUiStore(state => state.setAppState);
     const initializeNewGame = useGameStore(state => state.initializeNewGame);
+
 
 
     const startGame = () => {
@@ -19,12 +23,33 @@ export function OrgCreation() {
         setAppState('in_game');
     };
 
+
+    const successionOptions = Object.keys(SUCCESSION_CATALOG);
+
     return (
         <div>
             <label htmlFor="orgName">Name: </label>
             <input type="text" id="orgName" value={name} onChange={(e) => setName(e.target.value)} />
             <label htmlFor="orgColor">Color: </label>
             <input type="color" id="orgColor" value={color} onChange={(e) => setColor(e.target.value)} />
+
+            <div>
+                <label htmlFor="successionLaw">Succession Law:</label>
+                <select name="constructionTarget" value={selectedSuccession || ''} onChange={(e) => setSelectedSuccession(e.target.value as string)}>
+                    {successionOptions.map(successionKey => {
+                        if (!successionKey) return null;
+                        return(
+                            <option value={successionKey}>
+                                {successionKey}
+                            </option>);
+                    })}
+                </select>
+                <label htmlFor="termLength">Term Length in Years (0 = disabled)</label>
+                <input type="number" id="termLength" value={termLength} onChange={(e) => setTermLength(e.target.value)}/>
+            </div>
+
+
+
             <div>
                 <p>Species Info:</p>
                 <label htmlFor="speciesName">Name: </label>
